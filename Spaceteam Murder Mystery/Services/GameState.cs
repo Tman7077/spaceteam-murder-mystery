df5024c574d59f6a28cd71d034bfbcbf84878f72
Tyler Bartle <tman.bartleboy1@gmail.com>
2025-05-26 11:49:42 -0600

    Added public get/set for difficulty; actually load characters from the character folder instead of loading from Assets/ (and into a null HashSet) in vain.

--- Spaceteam Murder Mystery/Services/GameState.cs
+++ Spaceteam Murder Mystery/Services/GameState.cs
@@ -7,7 +7,7 @@ namespace SMM.Services
 {
     public class GameState
     {
-        public Dictionary<string, Character> Characters { get; } = null!;
+        public Dictionary<string, Character> Characters { get; } = [];
         public IDifficulty Difficulty { get; set; } = null!;
 
         public GameState(IDifficulty difficulty)
@@ -16,16 +16,25 @@ namespace SMM.Services
         }
         public void StartReset(IDifficulty? difficulty = null)
         {
-            Difficulty = difficulty ?? Difficulty;
+            ChangeDifficulty(difficulty);
             LoadCharacters();
+        }
+        public void ChangeDifficulty(IDifficulty? difficulty = null)
+        {
+            Difficulty = difficulty ?? Difficulty;
+        }
+        public string GetDifficulty()
+        {
+            return Difficulty.GetType().Name[1..];
         }
 
         private void LoadCharacters()
         {
             Characters.Clear();
             string assetDir = PathHelper.GetAssetDirectory();
+            string charDir = Path.Combine(assetDir, "Text", "Characters");
 
-            foreach (string characterFile in Directory.GetFiles(assetDir, "*.md"))
+            foreach (string characterFile in Directory.GetFiles(charDir, "*.md"))
             {
                 string characterName = Path.GetFileNameWithoutExtension(characterFile);
                 CharacterData data = Parser.ParseCharacter(characterName);
