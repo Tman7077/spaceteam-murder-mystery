fd39a8c3901f5afa02e41e99c16e5299fc10d5d6
Tyler Bartle <tman.bartleboy1@gmail.com>
2025-05-28 20:44:14 -0600

    A little visual reorganization & lining up

--- Spaceteam Murder Mystery/Services/Parser.cs
+++ Spaceteam Murder Mystery/Services/Parser.cs
@@ -18,21 +18,21 @@ namespace SMM.Services
             (string name, string role) = ParseNameAndRole(lines[0]);
 
             int mottoIndex = Array.FindIndex(lines, line => line.StartsWith("*\""));
-            string motto = lines[mottoIndex].Trim('*');
+            string motto   = lines[mottoIndex].Trim('*');
 
-            string profileImagePath = Path.Combine(AssetDir, "Images", "Portraits", $"{characterName}.png");
-            string csImagePath = Path.Combine(AssetDir, "Images", "Crime Scenes", $"{characterName}.png");
+            string profileImagePath = Path.Combine(AssetDir, "Images", "Portraits",    $"{characterName}.png");
+            string csImagePath      = Path.Combine(AssetDir, "Images", "Crime Scenes", $"{characterName}.png");
 
             string description = lines[mottoIndex + 1];
 
-            int deathIndex = Array.FindIndex(lines, line => line == "## DEATH") + 1;
+            int deathIndex    = Array.FindIndex(lines, line => line == "## DEATH") + 1;
             string deathStory = lines[deathIndex];
 
-            int cluesIndex = Array.FindIndex(lines, line => line == "## CLUES & HINTS") + 1;
-            int interviewsIndex = Array.FindIndex(lines, line => line == "## INTERVIEW RESPONSES") + 1;
+            int cluesIndex       = Array.FindIndex(lines, line => line == "## CLUES & HINTS") + 1;
+            int interviewsIndex  = Array.FindIndex(lines, line => line == "## INTERVIEW RESPONSES") + 1;
             int accusationsIndex = Array.FindIndex(lines, line => line == "## ACCUSATION RESPONSES") + 1;
 
-            string[] clueLines = lines[cluesIndex..(interviewsIndex-1)];
+            string[] clueLines  = lines[cluesIndex..(interviewsIndex-1)];
             HashSet<Clue> clues = ParseClues(clueLines);
 
             string[] interviewLines = lines[interviewsIndex..(accusationsIndex-1)];
@@ -59,14 +59,14 @@ namespace SMM.Services
             string[] lineswithEmpty = File.ReadAllLines(Path.Combine(AssetDir, "Text", "Overview.md"));
             string[] lines = [.. lineswithEmpty.Where(line => !string.IsNullOrWhiteSpace(line))];
 
-            int introIndex = Array.FindIndex(lines, line => line == "### Intro") + 1;
+            int introIndex       = Array.FindIndex(lines, line => line == "### Intro") + 1;
             int firstMurderIndex = Array.FindIndex(lines, line => line == "### First Murder Intro") + 1;
-            int stopIndex = Array.FindIndex(lines, line => line == "*There's probably going to be more here*");
+            int stopIndex        = Array.FindIndex(lines, line => line == "*There's probably going to be more here*");
 
-            string[] introLines = lines[introIndex..(firstMurderIndex - 1)];
+            string[] introLines       = lines[introIndex..(firstMurderIndex - 1)];
             string[] firstMurderLines = lines[firstMurderIndex..stopIndex];
 
-            string intro = string.Join("\n\n", introLines);
+            string intro       = string.Join("\n\n", introLines);
             string firstMurder = string.Join("\n\n", firstMurderLines);
 
             return new Story(intro, firstMurder);
@@ -79,19 +79,19 @@ namespace SMM.Services
         }
         private static HashSet<Clue> ParseClues(string[] lines)
         {
-            HashSet<Clue> clues = [];
+            HashSet<Clue> clues  = [];
             string characterName = "";
-            string imageFolder = "";
+            string imageFolder   = "";
             foreach (string line in lines)
             {
                 if (line.StartsWith('#'))
                 {
                     characterName = line.Trim('#', ' ');
-                    imageFolder = Path.Combine(AssetDir, "Images", "Clues", $"{characterName}");
+                    imageFolder   = Path.Combine(AssetDir, "Images", "Clues", $"{characterName}");
                 }
                 else if (line.StartsWith('-'))
                 {
-                    string[] parts = line[2..].Split(": ");
+                    string[] parts  = line[2..].Split(": ");
                     string clueName = parts[0].Trim('*', ' ');
                     string clueDesc = parts[1].Trim();
 
@@ -108,9 +108,9 @@ namespace SMM.Services
 
             for (int i = 0; i < lines.Length; i += 3)
             {
-                string name = RegexCharacterName().Replace(lines[i], "$1").Trim('#', ' ', ':');
+                string name     = RegexCharacterName().Replace(lines[i], "$1").Trim('#', ' ', ':');
                 string innocent = lines[i + 1].Trim('>', ' ');
-                string guilty = lines[i + 2].Trim('>', ' ');
+                string guilty   = lines[i + 2].Trim('>', ' ');
                 responses.Add(name, new ResponseSet(innocent, guilty));
             }
             return responses;
