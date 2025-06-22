bbee9498f4750979f554880f3909163856c833a9
Tyler Bartle <tman.bartleboy1@gmail.com>
2025-06-21 22:53:58 -0600

    Created constructors that validate character names.

--- Spaceteam Murder Mystery/Views/Helpers/Screen.cs
+++ Spaceteam Murder Mystery/Views/Helpers/Screen.cs
@@ -2,12 +2,33 @@ namespace SMM.Views.Helpers;
 
 public abstract record Screen
 {
-    public sealed record CrimeScene(string VictimName) : Screen;
-    public sealed record Difficulty : Screen;
-    public sealed record InspectChar(InterviewType Type, string Interviewee, string Victim) : Screen;
-    public sealed record InspectClue(Clue Clue) : Screen;
-    public sealed record NewGame : Screen;
+    public sealed record CrimeScene : Screen
+    {
+        public string VictimName { get; init; }
+        public CrimeScene(string VictimName)
+        {
+            Validator.ValidateShortCharacterName(VictimName);
+            this.VictimName = VictimName;
+        }
+    }
+    public sealed record Difficulty  : Screen;
+    public sealed record InspectChar : Screen
+    {
+        public InterviewType Type { get; init; }
+        public string Interviewee { get; init; }
+        public string Victim { get; init; }
+        public InspectChar(InterviewType Type, string Interviewee, string Victim)
+        {
+            Validator.ValidateShortCharacterName(Interviewee);
+            Validator.ValidateShortCharacterName(Victim);
+            this.Type = Type;
+            this.Interviewee = Interviewee;
+            this.Victim = Victim;
+        }
+    }
+    public sealed record InspectClue(Clue Clue)        : Screen;
+    public sealed record NewGame  : Screen;
     public sealed record Selection(InterviewType Type) : Screen;
     public sealed record Settings : Screen;
-    public sealed record Title : Screen;
+    public sealed record Title    : Screen;
 }
