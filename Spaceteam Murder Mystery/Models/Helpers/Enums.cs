9388e44dd09808bb270a40689547acadc8737c3d
Tyler Bartle <tman.bartleboy1@gmail.com>
2025-06-28 09:35:09 -0600

    Enums with only a few values have no reason to be stored as 32-bit ints, so now they're bytes :).

--- Spaceteam Murder Mystery/Models/Helpers/Enums.cs
+++ Spaceteam Murder Mystery/Models/Helpers/Enums.cs
@@ -1,6 +1,6 @@
 namespace SMM.Models.Helpers;
 
-public enum Direction { Left, Right }
-public enum FadeType { In, Out }
-public enum InterviewType { Interview, Accusation }
-public enum SoundtrackType { TitleTheme, MainTheme }
+public enum Direction      : byte { Left, Right }
+public enum FadeType       : byte { In, Out }
+public enum InterviewType  : byte { Interview, Accusation }
+public enum SoundtrackType : byte { TitleTheme, MainTheme }
