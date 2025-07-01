c2c2744e1c39f4626c4be89ff7da522699ef7543
Tyler Bartle <tman.bartleboy1@gmail.com>
2025-07-01 15:16:57 -0600

    The title screen now has an animated gradient, and the row definitions match the difficulty screen so the header doesn't jump around

--- Spaceteam Murder Mystery/Views/Static/TitleScreen.xaml.cs
+++ Spaceteam Murder Mystery/Views/Static/TitleScreen.xaml.cs
@@ -1,5 +1,7 @@
 namespace SMM.Views.Static;
 
+using System.Windows.Media.Animation;
+
 public partial class TitleScreen : UserControl
 {
     private readonly MainWindow _main;
@@ -11,8 +13,9 @@ public partial class TitleScreen : UserControl
         Background = new ImageBrush()
         {
             ImageSource = AssetHelper.NebulaBG,
-            Stretch     = Stretch.UniformToFill
+            Stretch = Stretch.UniformToFill
         };
+        AnimateTitleGradient();
     }
 
     private async void StartGame_Click(object sender, RoutedEventArgs e) =>
@@ -26,4 +29,48 @@ public partial class TitleScreen : UserControl
 
     private void Quit_Click(object sender, RoutedEventArgs e) =>
         Application.Current.Shutdown();
+
+    public void AnimateTitleGradient()
+    {
+        LinearGradientBrush gradient = new()
+        {
+            StartPoint    = new Point(0, 0),
+            EndPoint      = new Point(1, 0),
+            MappingMode   = BrushMappingMode.RelativeToBoundingBox,
+            SpreadMethod  = GradientSpreadMethod.Reflect,
+            GradientStops =
+            [
+                new GradientStop((Color)ColorConverter.ConvertFromString("#C25A0C"), 0.0),
+                new GradientStop((Color)ColorConverter.ConvertFromString("#BD8C00"), 0.25),
+                new GradientStop((Color)ColorConverter.ConvertFromString("#7CADB2"), 0.5),
+                new GradientStop((Color)ColorConverter.ConvertFromString("#036784"), 0.75),
+                new GradientStop((Color)ColorConverter.ConvertFromString("#001C42"), 1.0),
+                new GradientStop((Color)ColorConverter.ConvertFromString("#036784"), 1.25),
+                new GradientStop((Color)ColorConverter.ConvertFromString("#7CADB2"), 1.5),
+                new GradientStop((Color)ColorConverter.ConvertFromString("#BD8C00"), 1.75),
+                new GradientStop((Color)ColorConverter.ConvertFromString("#C25A0C"), 2.0),
+            ]
+        };
+
+        // RotateTransform    rt    = new(30, 0.5, 0.5);
+        TranslateTransform tt    = new();
+        // TransformGroup     group = new();
+        // group.Children.Add(rt);
+        // group.Children.Add(tt);
+
+        // gradient.RelativeTransform = group;
+        gradient.RelativeTransform = tt;
+
+        TitleTextBlock.Foreground = gradient;
+
+        var animation = new DoubleAnimation
+        {
+            From           = 0,
+            To             = gradient.GradientStops.Max(gs => gs.Offset),
+            Duration       = TimeSpan.FromSeconds(15),
+            RepeatBehavior = RepeatBehavior.Forever,
+        };
+
+        tt.BeginAnimation(TranslateTransform.XProperty, animation);
+    }
 }
